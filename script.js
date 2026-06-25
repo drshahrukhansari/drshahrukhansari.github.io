853e7970a261fbb5cb73300ee9645cfd307f0eba
drshahrukhansari <iamshahrukhansarii@gmail.com>
2026-06-25 15:44:31 +0530

    Add event listener to appointment form submission

---
+++ script.js
@@ -0,0 +1,12 @@
+document
+.getElementById("appointmentForm")
+.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    window.open(
+        "https://wa.me/916377289139",
+        "_blank"
+    );
+
+});
