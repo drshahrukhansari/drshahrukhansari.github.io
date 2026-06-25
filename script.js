21d079d56f56e5ed622938e09bb78639c7ba9608
drshahrukhansari <iamshahrukhansarii@gmail.com>
2026-06-25 15:59:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-document
-.getElementById("appointmentForm")
-.addEventListener("submit", function (e) {
+document.getElementById("appointmentForm").addEventListener("submit", function(e) {
 
     e.preventDefault();
 
-    window.open(
-        "https://wa.me/916377289139",
-        "_blank"
-    );
+    let name = document.querySelector('input[placeholder="Patient Name"]').value;
+    let mobile = document.querySelector('input[placeholder="Mobile Number"]').value;
+    let age = document.querySelector('input[placeholder="Age"]').value;
+    let service = document.querySelector('select').value;
+    let problem = document.querySelector('textarea').value;
+
+    let message =
+`*New Appointment Request*
+
+👤 Name: ${name}
+📞 Mobile: ${mobile}
+🎂 Age: ${age}
+🏠 Service: ${service}
+🩺 Problem: ${problem}`;
+
+    let whatsappURL =
+    `https://wa.me/916377289139?text=${encodeURIComponent(message)}`;
+
+    window.open(whatsappURL, "_blank");
 
 });
